4a6378c26f7dd5eb3c7e4ffaee883ad5bfae1d0a
SzaboGabor123 <71808214+SzaboGabor123@users.noreply.github.com>
2020-11-13 16:48:45 +0100

    kiértékelő lap hozzáadása

---
+++ js1.js
@@ -0,0 +1,18 @@
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+function kiertekeles(){
+    var num1, num2, num3,
+    num1=Number(document.formcalc.feladat1.value),
+    num2=Number(document.formcalc.feladat2.value),
+    num3=Number(document.formcalc.feladat3.value),
+    res=num1+num2+num3,
+    document.formcalc.txtres.value=res;
+}
