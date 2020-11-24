8313e8985632e2a28fdbffe3aef5e4bb3f6bba6b
SzaboGabor123 <71808214+SzaboGabor123@users.noreply.github.com>
2020-11-24 19:02:54 +0100

    Az oldal reszponzívvá tétele, illetve a kiértékelés is működik (js).

--- js1.js
+++ js1.js
@@ -7,12 +7,34 @@ function revealMessage() {
 function revealMessage() {
     document.getElementById("hiddenMessage").style.display = 'block';
 }
-
 function kiertekeles(){
-    var num1, num2, num3,
-    num1=Number(document.formcalc.feladat1.value),
-    num2=Number(document.formcalc.feladat2.value),
-    num3=Number(document.formcalc.feladat3.value),
-    res=num1+num2+num3,
-    document.formcalc.txtres.value=res;
+
+    var q = parseInt(document.getElementById("a").value);
+    var w = parseInt(document.getElementById("b").value);
+    var e = parseInt(document.getElementById("c").value);
+
+    var elert = q + w + e;
+    var szazalek = elert / 18 * 100;
+
+    document.getElementById("eredmeny").innerHTML = "Az eredmény: " + elert + " pont, " + szazalek + "%";
+
+    if (szazalek > 80){
+        document.getElementById("jegy").innerHTML = "5 (JELES)"
+    }
+
+    else if (szazalek > 60){
+        document.getElementById("jegy").innerHTML = "4 (JÓ)"
+    }
+
+    else if (szazalek > 40){
+        document.getElementById("jegy").innerHTML = "3 (KÖZEPES)"
+    }
+
+    else if (szazalek > 25){
+        document.getElementById("jegy").innerHTML = "2 (ELÉGSÉGES)"
+    }
+
+    else{
+        document.getElementById("jegy").innerHTML = "1 (ELÉGTELEN)"
+    }
 }
